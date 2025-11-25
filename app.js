89cb5ac60987f34ac87cde2dbd4ac0896cfc1f9a
nicolecamposs <nicolecampos6@hotmail.com>
2025-11-25 17:40:12 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 tentativas = 1;
 
@@ -36,7 +36,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Digite um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Digite um número entre 1 e 100');
 }
 
 function gerarNumeroAleatorio() {
